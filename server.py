4f821386bb5ab901db851635094ae628b48e0f47
Valeria1999 <cernavcavaleria@gmail.com>
2020-12-17 19:19:40 +0200

    update server

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import random
 import sys
+import clientIdentityClass
 
 LOCALHOST = "127.0.0.1"
 PORT = 1887
@@ -10,14 +11,6 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((LOCALHOST, PORT))
 print("[ SERVER ] Let's go")
 print("[ SERVER ] Waiting for client's requests ...")
-
-
-class ClientIdentity:
-    def __init__(self,sockCl,nameCl,preferenceCl):
-        self.sockCl = sockCl
-        self.name = nameCl.upper()
-        self.preference = preferenceCl
-        self.score = []
 
 class Thread(threading.Thread):
     def __init__(self,clientAdr,sockCl):
@@ -31,7 +24,19 @@ class Thread(threading.Thread):
         recvName = self.sockCl.recv(2048)
         clientNichName = recvName.decode()
         print ("[ CLIENT ] My name is ",clientNichName)
-
+        preference = 'none'
+        clientIdentity = clientIdentityClass.ClientIdentity(self.sockCl,clientNichName,preference)
+        
+        #ask preference
+        self.sockCl.send(bytes("[ SERVER ] Do you want to play with a friend? y/n",'UTF-8'))
+        recvPreference = self.sockCl.recv(2048)
+        recvPrefere = recvPreference.decode()
+        if recvPrefere=='y':
+            print ("[",clientNichName,"] I want to play with a friend! ")
+            clientIdentity.preference = 'multiPlayer'
+        elif recvPrefere=='n':
+            print ("[",clientNichName.upper(),"] I want to play with server! ")
+            clientIdentity.preference = 'onePlayer'
 
 while True:  
     server.listen(1)
